29ac28c052df2305309bf43b6e83521caeb4871f
luicar <carlos_mendoza190@hotmail.com>
2020-02-04 23:38:13 -0500

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+//Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+
+//Componente
 import { AppComponent } from './app.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { AddContactComponent } from './components/schedule/add-contact/add-contact.component';
+import { ListContactsComponent } from './components/schedule/list-contacts/list-contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    AddContactComponent,
+    ListContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
